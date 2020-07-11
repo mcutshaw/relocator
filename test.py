b97fa43b2124c9da3270ea4db9d6f9540508c5a8
mcutshaw <jack@jackofnotrades.org>
2020-07-11 16:50:40 -0500

    deleted test

--- test.py
+++
@@ -1,8 +0,0 @@
-from block import BlockGenerator
-from configparser import ConfigParser
-
-if __name__ == '__main__':
-
-    config = ConfigParser()
-    config.read('config.txt')
-    blocks = BlockGenerator(config).blocks
